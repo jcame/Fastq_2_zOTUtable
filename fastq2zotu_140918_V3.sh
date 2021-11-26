7af4e5a93d05ae5a37dc01ee02f1ab5ddfe4d3e2
Josue Castro <jcame@food.ku.dk>
2021-11-26 09:19:10 +0100

    Update fastq2zotu_140918_V3.sh

--- fastq2zotu_140918_V3.sh
+++ fastq2zotu_140918_V3.sh
@@ -1,20 +1,14 @@
-#>>>>>>>>>>By Krych & Castro-Mejía 2018 <<<<<<<<<<<<<<
+#>>>>>>>>>>By Krych & Castro-Mejía 2021 <<<<<<<<<<<<<<
 #>>>>>>>>>>License : University of Copenhagen<
 #>>>>>>>>>>krych@food.ku.dk, jcame@food.ku.dk<<<<<<<<<<<<<<<<<<<
 
-#>>>>>>>>>>Merge reads<<<<<<<<<<<<<<<<<<<<<<<<
 
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
-
+    #Adjusting file labels, relabeling sequences & merging_ovelaping_pairs##############################################################################
 
 reverse_file=(*R2*.fastq)
-
 for file1 in *R1*.fastq
 do
 
-#>>>>>>>>adjusting label<<<<<<<<<<<<<<<<<<<<
-tput setaf 4
 string=${file1}
 string2=$(sed "-es/_R[0-9].*\.fastq//; s/combined_//" <<< $string)
 string3=$(sed "-es/-/_/g" <<< $string2)
@@ -24,186 +18,92 @@ string4=$(sed "-es/S_[0-9].//g" <<< $string3)
 echo ""
 echo "project name: ${string4}"
 echo ""
-tput sgr0
 
-#>>>>>>>>merging_ovelaping_pairs<<<<<<<<<<<<<<<<
-usearch10 -fastq_mergepairs ${file1} -reverse ${reverse_file[i++]} -fastqout merged-${file1}.fq -fastq_minovlen 100 -relabel ${string4}.
-#staggered reads arte removed by default in usearch10
+usearch -fastq_mergepairs ${file1} -reverse ${reverse_file[i++]} -fastqout merged-${file1}.fq -fastq_minovlen 100 -relabel ${string4}.
+
 done
 
-tput setaf 4
-echo ""
-echo ">>>>merging fastq together<<<<"
-#####################################cat merged-combined*fq > merged.fq
-echo ""
+    #Cleaning - moving fastq files to a storage directory##############################################################################
 
-#cleaning
-#####################################rm merged-combined*.fq
 mkdir ${string4}
 mv *.fastq ${string4}/
 
+    #Quality check of merged sequences##############################################################################
 
-#>>>>>>>Quality check<<<<<<<<<<<<<<<<<<<<<<<<<<<
-echo ""
-echo ">>>>checking fastq quality<<<<"
-echo ""
-tput sgr0
 for file in merged*fq
 do
-usearch61 -fastq_filter $file -fastq_maxee 2.0  -fastq_truncqual 4 -fastq_minlen 130 -fastaout ${file}.fna
+usearch -fastq_filter $file -fastq_maxee 2.0  -fastq_truncqual 4 -fastq_minlen 130 -fastaout ${file}.fna
 done
-tput setaf 4
-echo ""
-echo ">>>>merged fastq filtered by quality<<<<"
-tput sgr0
+
+    #Concatenating high quality merged reads
 
 cat merged*.fna > qual-filt.fna
 rm merged*.fna
 rm merged*.fq
 
-#>>>>>>>Find unique read sequences<<<<<<<<<<<<<<
-tput setaf 4
-echo ""
-echo ">>>>searching unique sequences<<<<"
-tput sgr0
-usearch10 -fastx_uniques qual-filt.fna -sizeout -fastaout uniques.fa
+    #Finding unique sequences##############################################################################
 
+usearch -fastx_uniques qual-filt.fna -sizeout -fastaout uniques.fa
 
-usearch10 -unoise3 uniques.fa -zotus zotus.fa
+    #Identifying zero-radious OTUs and removing chimeric sequences
+
+usearch -unoise3 uniques.fa -zotus zotus.fa
 sed "s/Zotu/zOTU_/g" zotus.fa > zotus2.fa 
 rm zotus.fa
+rm uniques.fa
 
-usearch10 -otutab qual-filt.fna -otus zotus2.fa -otutabout raw_zOTU.txt  -id 0.999
+    #Building an OTU table##############################################################################
+
+usearch -otutab qual-filt.fna -otus zotus2.fa -otutabout raw_zOTU.txt  -id 0.999
 cat raw_zOTU.txt | sed 's/#OTU ID/OTUID/g' > raw_zOTU2.txt 
 rm raw_zOTU.txt
+rm qual-filt.fna
 
-usearch10 -sintax zotus2.fa -db '/mnt/ed058f9c-275f-4599-a283-e4e5927a7946/Databases_16s/rdp_16s_v16s_sp.fa' -strand both -tabbedout sintax0.txt -sintax_cutoff 0.8
-usearch10 -sintax zotus2.fa -db '/mnt/ed058f9c-275f-4599-a283-e4e5927a7946/Databases_16s/Ez-taxon-format-sintax.fasta'  -strand both -tabbedout sintaxEt.txt -sintax_cutoff 0.8
-TAB=$'\t'
-cat sintax0.txt | awk '{print $1,$4}'| sed 's/"//g' | sed 's/p:/p__/g' | sed 's/c:/c__/g' | sed 's/o:/o__/g' | sed 's/f:/f__/g' | sed 's/g:/g__/g' | sed 's/s:/s__/g' | sed 's/,/;/g' | sed 's/ d:Bacteria;/'"${TAB}k__Bacteria;"'/g' | sed 's/ d:Archaea;/'"${TAB}k__Archaea;"'/g' > sintax.txt 
+    #Taxonomic affiliation of zOTUs##############################################################################
 
-cat sintaxEt.txt | awk -F "\t" '{print $1,$4}'| sed 's/"//g' | sed 's/p:/p__/g' | sed 's/c:/c__/g' | sed 's/o:/o__/g' | sed 's/f:/f__/g' | sed 's/g:/g__/g' | sed 's/s:/s__/g' | sed 's/,/;/g' | sed 's/ d:Bacteria;/'"${TAB}k__Bacteria;"'/g' | sed 's/d:Bacteria/'"${TAB}k__Bacteria;"'/g' | sed 's/ d:Archaea;/'"${TAB}k__Archaea;"'/g' > Etsintax.txt 
+usearch -sintax zotus2.fa -db Databases_16s/Ez-taxon-format-sintax.fasta  -strand both -tabbedout sintaxEt.txt -sintax_cutoff 0.8
+cat sintaxEt.txt | cut -f1,4 | sed 's/d:/k__/g' | sed 's/p:/p__/g' | sed 's/c:/c__/g' | sed 's/o:/o__/g' | sed 's/f:/f__/g' | sed 's/g:/g__/g' | sed 's/s:/s__/g' | sed 's/,/;/g' > Etsintax.txt 
 
-
-rm sintax0.txt
 rm sintaxEt.txt
+TAB=$'\t'
 
-echo "OTUID$TAB taxonomy" > header.txt 
-cat header.txt | sed 's/ //g' > headers.txt 
-cat headers.txt sintax.txt > sintaxR.txt
-cat headers.txt Etsintax.txt > EtsintaxR.txt
-rm headers.txt
-rm header.txt
-sed 's/#OTU ID/OTUID/g' < raw_zOTU2.txt > raw_zOTU_R2.txt
+echo 'OTUID'"${TAB}"'taxonomy' > headers.txt 
+cat headers.txt Etsintax.txt  > EtsintaxR.txt ; rm  headers.txt ; rm Etsintax.txt 
+
+
+echo '#!/usr/bin/env Rscript
+X = read.table("raw_zOTU2.txt", header = TRUE, sep = "\t")
+Y <- read.table("EtsintaxR.txt", header = TRUE, sep = "\t", na.string = "")
+table <- merge(X,Y, by = "OTUID")
+write.table(table, "OTU_completed_EZtaxon.txt", sep="\t", row.names = F, quote =F)' > biom.R
+
+chmod 755 biom.R
+./biom.R
+rm biom.R
 rm raw_zOTU2.txt
-
 
 mkdir Results_${string4}
 mkdir Results_${string4}/OTU-tables
+mv OTU_completed_EZtaxon.txt Results_${string4}/OTU-tables/
 
+    #Generating phylogenetic tree ##############################################################################
 
-echo '#!/usr/bin/env Rscript
-
-X = read.table("raw_zOTU_R2.txt", header = TRUE)
-Y <- read.table("sintaxR.txt", header = TRUE, fill = TRUE, na.string = "")
-table <- merge(X,Y, by = "OTUID")
-write.table(table, "OTU_completed.txt", sep="\t", row.names = F)' > biom.R
-
-chmod 755 biom.R
-./biom.R
-
-
-echo '#!/usr/bin/env Rscript
-
-X = read.table("raw_zOTU_R2.txt", header = TRUE)
-Y <- read.table("EtsintaxR.txt", header = TRUE, fill = TRUE, na.string = "", sep="\t")
-table <- merge(X,Y, by = "OTUID")
-write.table(table, "OTU_completedEt.txt", sep="\t", row.names = F)' > biom3.R
-
-chmod 755 biom3.R
-./biom3.R
-
-
-cat OTU_completed.txt | sed 's/"//g' | sed 's/OTUID/#OTU ID/g' | sed 's/NA/Unassigned/g' > zOTU_table_sintax.txt
-biom convert -i zOTU_table_sintax.txt -o Results_${string4}/OTU-tables/zOTU_table_sintax.biom --process-obs-metadata taxonomy --table-type="OTU table" --to-json
-
-cat OTU_completedEt.txt | sed 's/"//g' | sed 's/OTUID/#OTU ID/g' | sed 's/+/Unassigned/g' > zOTU_table_sintaxEt.txt
-biom convert -i zOTU_table_sintaxEt.txt -o Results_${string4}/OTU-tables/zOTU_table_sintaxEt.biom --process-obs-metadata taxonomy --table-type="OTU table" --to-json
-
-mv zOTU_table_sintax.txt Results_${string4}/OTU-tables/
-mv zOTU_table_sintaxEt.txt Results_${string4}/OTU-tables/
-
-tput setaf 4
-echo ""
-echo ">>>>generating phylogenetic tree<<<<"
-tput sgr0
 mkdir Results_${string4}/trees
-usearch10 -cluster_agg zotus2.fa  -treeout tmp.tre -id 0.75 -linkage min
-
-
+usearch -calc_distmx zotus2.fa -tabbedout mx.txt 
+usearch -cluster_aggd  mx.txt   -treeout tmp.tre -linkage min
 tr -d "\n" < tmp.tre | sed "-es/zOTU_/'zOTU_/g" | sed "-es/:/':/g" | sed "-es/)':/):/g" > Results_${string4}/trees/zOTU.tree 
-
-mkdir Results_${string4}/zOTUs
-
-
-assign_taxonomy.py -i zotus2.fa -o assign_output -r /mnt/ed058f9c-275f-4599-a283-e4e5927a7946/Databases_16s/gg_13_8_otus/rep_set/97_otus.fasta  -t /mnt/ed058f9c-275f-4599-a283-e4e5927a7946/Databases_16s/gg_13_8_otus/taxonomy/97_otu_taxonomy.txt
-
-echo "OTUID$TAB taxonomy$TAB hola$TAB tigre" > header.txt 
-cat header.txt | sed 's/ //g' > headers.txt
-cat headers.txt assign_output/zotus2_tax_assignments.txt | sed 's/ //g' > greengenesR.txt
-rm headers.txt
-rm header.txt
-
-echo '#!/usr/bin/env Rscript
-
-X = read.table("raw_zOTU_R2.txt", header = TRUE)
-Z <- read.table("greengenesR.txt", header = TRUE, fill = TRUE, na.string = "")
-Y = Z[,c(1,2)]
-table <- merge(X,Y, by = "OTUID")
-write.table(table, "OTU_completed.txt", sep="\t", row.names = F)' > biom.R
-
-chmod 755 biom.R
-./biom.R
-
-cat OTU_completed.txt | sed 's/"//g' | sed 's/OTUID/#OTU ID/g' | sed 's/NA/Unassigned/g' > zOTU_table_GG.txt
-biom convert -i zOTU_table_GG.txt -o Results_${string4}/OTU-tables/zOTU_table_GG.biom --process-obs-metadata taxonomy --table-type="OTU table" --to-json
+rm mx.txt 
+rm tmp.tre
 
 #>>>>>>move ZOTUs
-mv zOTU_table_GG.txt Results_${string4}/OTU-tables/
+
+mkdir Results_${string4}/zOTUs
 cp zotus2.fa Results_${string4}/zOTUs/zOTUs.txt
 mv zotus2.fa Results_${string4}/zOTUs/zOTUs.fa
 
 mkdir Results_${string4}/taxonomy
-mv sintaxR.txt Results_${string4}/taxonomy/sintax_taxaRDP.txt
-mv greengenesR.txt Results_${string4}/taxonomy/greengenes_taxa.txt
-mv EtsintaxR.txt Results_${string4}/taxonomy/sintax_taxaEt.txt
+mv EtsintaxR.txt Results_${string4}/taxonomy/sintax_EZtaxon.txt
 
-rm -r assign_output/
-
-notify-send "ANALYSIS COMPLEATED. Thank you! Krych & Castro-Mejía ;-) 2018"
-
-
-
-tput setaf 4
-echo ""
-read -p ">>>>>>Would you like to remove half-products  (y/n) ?"  choice
-if [ "$choice" == "y" ]; then
-
-
-rm qual-filt.fna 
-rm uniques.fa
-rm sintax.txt
-rm tmp.tre
-rm biom*.R
-rm OTU_completed.txt
-rm raw_zOTU_R2.txt
-rm Etsintax*
-rm OTU_completedEt.txt
-
-elif [ "$choice" == "n" ]; then
-echo " O.K. files will be kept"
-fi
-tput sgr0
 
 
 
